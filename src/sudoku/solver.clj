053fa77313f0a46730657fbf8f43c16fc84e3ce6
Andrew Jackson <andrewsyoga@gmail.com>
2018-03-09 22:46:19 +0000

    Adding initial stage of a map-indexed solution attempt

--- src/sudoku/solver.clj
+++ src/sudoku/solver.clj
@@ -11,6 +11,23 @@
       _ 3 4 _ _ _ 1 _ _
       9 _ _ 8 _ _ _ 5 _
       _ _ _ 4 _ _ 3 _ 7]))
+(map-indexed vector sudoku)
+(map-indexed
+  (fn [idx itm]
+    (if (not (nil? itm))
+      itm
+      ;[(int (/ idx 9)) (mod idx 9)]
+      ;(map #(nth sudoku %) (map #(+ (mod idx 9) (* 9 %)) (range 9)));col values
+      ;(map #(nth sudoku %) (map #(+ % (* 9 (int (/ idx 9)))) (range 9)));row values
+      (remove
+      (into #{}
+        (remove nil?
+          (concat
+            (map #(nth sudoku %) (map #(+ (mod idx 9) (* 9 %)) (range 9)))
+            (map #(nth sudoku %) (map #(+ % (* 9 (int (/ idx 9)))) (range 9))))))
+        (set (range 1 10)))
+      ))
+  sudoku)
 (get-options-set (partition 9 sudoku) sudoku)
 (defn get-options-set
   "Takes an initial unsolved sudoku and returns a map with keys for each location
