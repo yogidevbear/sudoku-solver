412db5aaadefd67598522eabde4ab5dd077a5183
Andrew Jackson <andrewsyoga@gmail.com>
2018-03-11 23:28:27 +0000

    Updating get-options-set function for calculating potential options from exclusion logic using row, column and 3x3 mini-grid data

--- src/sudoku/solver.clj
+++ src/sudoku/solver.clj
@@ -12,47 +12,38 @@
       9 _ _ 8 _ _ _ 5 _
       _ _ _ 4 _ _ 3 _ 7]))
 (map-indexed vector sudoku)
-(map-indexed
-  (fn [idx itm]
-    (if (not (nil? itm))
-      itm
-      (first (remove nil? (map #(if (some #{idx} %) %) (partition 9 (range 81)))))
-      ))
-  sudoku)
-      ;[(int (/ idx 9)) (mod idx 9)]
-      ;(map #(nth sudoku %) (map #(+ (mod idx 9) (* 9 %)) (range 9)));col values
-      ;(map #(nth sudoku %) (map #(+ % (* 9 (int (/ idx 9)))) (range 9)));row values
-      (remove
-      (into #{}
-        (remove nil?
-          (concat
-            (map #(nth sudoku %) (map #(+ (mod idx 9) (* 9 %)) (range 9)))
-            (map #(nth sudoku %) (map #(+ % (* 9 (int (/ idx 9)))) (range 9))))))
-        (set (range 1 10)))
-      ))
-  sudoku)
-(get-options-set (partition 9 sudoku) sudoku)
 (defn get-options-set
   "Takes an initial unsolved sudoku and returns a map with keys for each location
   with possible options to use"
-  [grid sudoku]
-  (let [all-options-set (set (range 1 10))
-        col-values (apply map vector grid)]
-    (for [y (range 9)]
-      (let [row-values (nth grid y)]
-        (for [x (range 9)]
-          (if (not (nil? (nth sudoku (+ x (* y 9)))))
-            (nth sudoku (+ x (* y 9)))
-            (remove
-              (into #{} (remove nil? (concat (nth col-values x) row-values)))
-              all-options-set)))))))
+  [sudoku]
+  (map-indexed
+    (fn [idx itm]
+      (if (not (nil? itm))
+        itm
+        (let [threes (partition 3 (range 81))
+              mini-grids [(mapcat #(nth threes %) [0 3 6])
+                          (mapcat #(nth threes %) [1 4 7])
+                          (mapcat #(nth threes %) [2 5 8])
+                          (mapcat #(nth threes %) [9 12 15])
+                          (mapcat #(nth threes %) [10 13 16])
+                          (mapcat #(nth threes %) [11 14 17])
+                          (mapcat #(nth threes %) [18 21 24])
+                          (mapcat #(nth threes %) [19 22 25])
+                          (mapcat #(nth threes %) [20 23 26])]]
+          (remove
+            (into #{}
+              (remove nil?
+                (concat
+                  (map #(nth sudoku %) (map #(+ (mod idx 9) (* 9 %)) (range 9)))
+                  (map #(nth sudoku %) (map #(+ % (* 9 (int (/ idx 9)))) (range 9)))
+                  (map #(nth sudoku %) (first (filter #(contains? (set %) idx) mini-grids))))))
+            (set (range 1 10))))))
+    sudoku))
+      ;[(int (/ idx 9)) (mod idx 9)]
+      ;(map #(nth sudoku %) (map #(+ (mod idx 9) (* 9 %)) (range 9)));col values
+      ;(map #(nth sudoku %) (map #(+ % (* 9 (int (/ idx 9)))) (range 9)));row values
 
-
-            ;:let [value (nth (nth grid x) y)))
-
-    (if (not nil? value)
-      value
-      (let ))))
+(get-options-set sudoku)
 
 (defn solve
   "Takes a sequence of numbers representing a possibly incomplete Sudoku puzzle
